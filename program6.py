1aaca2233d4846d76c4c42f9206e0e39e6bd2184
laboni-dev <ldas11780@gmail.com>
2026-05-09 22:57:08 +0530

    Create program6.py

---
+++ program6.py
@@ -0,0 +1,8 @@
+marks = int(input("তোমার মার্কস কত? "))  # ✅ একদম বাম দিকে
+
+if marks >= 40:                         # ✅ বাম দিকে
+    print("Pass করেছো")                  # ✅ Tab দিয়ে সরানো
+    print("Congrats Laboni!")           # ✅ Tab দিয়ে সরানো
+else:                                   # ✅ বাম দিকে  
+    print("Fail করেছো")                 # ✅ Tab দিয়ে সরানো
+    print("আরো পড়তে হবে")              # ✅ Tab দিয়ে সরানো
