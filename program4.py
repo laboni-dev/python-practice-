8a4ba8ea56d1da05daa5e923397403732fb92a7f
laboni-dev <ldas11780@gmail.com>
2026-05-09 18:56:17 +0530

    Update program4.py

--- program4.py
+++ program4.py
@@ -1,6 +1,6 @@
 name = input("তোমার নাম কি? ")
 age = input("তোমার বয়স কত? ")
 
-print("labani)
-print("19"বছর")
+print("হ্যালো", name)
+print("তোমার বয়স", age, "বছর")
 print("তুমি AI Developer হতে পারবে!")
