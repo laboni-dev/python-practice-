83062bbc8fabb546e522c7fb6790ef3fe8474be2
laboni-dev <ldas11780@gmail.com>
2026-05-10 16:22:33 +0530

    Create practice3.py

---
+++ practice3.py
@@ -0,0 +1,9 @@
+name = input("তোমার নাম কি?")
+age = input("বয়স কত?")
+dream = input("স্বপ্ন কি?")
+
+print("---  তোমার বায়ো ---")
+print("নাম:", name)
+print("বয়স:", age)
+print("স্বপ্ন:",dream) 
+print("তুমি পারবে 💚")
