571643d60f663154faa8c651b8befc92c3f53ffc
laboni-dev <ldas11780@gmail.com>
2026-05-10 14:28:04 +0530

    Create program10.py

---
+++ program10.py
@@ -0,0 +1,14 @@
+marks = int(input("নম্বর দাও: "))
+
+if marks >= 90:
+    print("গ্রেড: A+")
+elif marks >= 80:
+    print("গ্রেড: A")
+elif marks >= 70:
+    print("গ্রেড: B")
+elif marks >= 60:
+    print("গ্রেড: C")
+elif marks >= 40:
+    print("গ্রেড: D")
+else:
+    print("গ্রেড: F - ফেল")
