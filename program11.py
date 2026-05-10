bd6c352c916e9eb1aeed5692e4e885d19c99fe5e
laboni-dev <ldas11780@gmail.com>
2026-05-10 15:40:39 +0530

    Create program11.py

---
+++ program11.py
@@ -0,0 +1,9 @@
+name = input("তোমার নাম কী? ")
+age = input("বয়স কত? ")
+dream = input("স্বপ্ন কী? ")
+
+print("--- তোমার বায়ো ---")
+print("নাম:", name)
+print("বয়স:", age)
+print("স্বপ্ন:", dream)
+print("তুমি পারবে 💚")
